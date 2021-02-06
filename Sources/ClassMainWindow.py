d405ed977b9fa26c1a821bcc1ea166cc1f47a5c2
Ruby <nicolas.da-silva@epitech.eu>
2021-02-07 00:48:13 +0100

    {[+] learn [+]}

--- Sources/ClassMainWindow.py
+++ Sources/ClassMainWindow.py
@@ -21,26 +21,81 @@ class MainWindow(QtWidgets.QMainWindow):
         self.setWindowTitle("TODO")
         self.setWindowIcon(QtGui.QIcon('TODO'))
 
+    def get_data(self):
+        loc = requests.get("https://ipinfo.io/json")
+        city = loc.json()['city']
+        loc = loc.json()['loc']
+        loc = loc.split(',')
+
+        getter_api = requests.get("http://api.waqi.info/feed/geo:" + loc[0] + ";" + loc[1] + "/?token=fbd6653ea37a6ad41658f86ea896e3c5f22a31f0")
+        if getter_api.json()['status'] != "error":
+            pol = getter_api.json()['data']['iaqi']
+            dico = {}
+            for i in range(len(pol)):
+                dico[list(pol)[i]] = list(list(pol.values())[i].values())[0]
+            m = folium.Map(location=loc, zoom_start=15, no_touch=True)
+            city_marker = folium.Marker(location=loc, popup=city)
+            m.add_child(city_marker)
+        return m
+        
+
     def load_map(self):
         widget = QtWidgets.QWidget()
         layout = QtWidgets.QVBoxLayout()
-
-        getter_api = requests.get("http://api.waqi.info/feed/here/?token=fbd6653ea37a6ad41658f86ea896e3c5f22a31f0")
-        locat = getter_api.json()['data']['city']['geo']
-        city = getter_api.json()['data']['city']['name']
-
-        m = folium.Map(location=locat, zoom_start=15, no_touch=True)
-        city_marker = folium.Marker(location=locat, popup=city)
-        m.add_child(city_marker)
-
         data = io.BytesIO()
-        m.save(data, close_file=False)
-
+        data_map = self.get_data()
+        data_map.save(data, close_file=False)
         web_view = QWebEngineView()
         web_view.setHtml(data.getvalue().decode())
         widget.setLayout(layout)
         layout.addWidget(web_view)
         self.setCentralWidget(widget)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     def __init__(self):
         super().__init__()
