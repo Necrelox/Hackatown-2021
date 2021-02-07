9d182ebaa467b9609cef70f2f9601f5e19ab8f00
Ruby <nicolas.da-silva@epitech.eu>
2021-02-07 03:41:14 +0100

    {[+]  Add Loading  [+]}

---
+++ Sources/ClassSplashLoad.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+##
+## EPITECH PROJECT, 2021
+## Hackatown-2021
+## File description:
+## ClassSplashLoad
+##
+
+from PySide2 import QtCore, QtGui, QtWidgets
+from PySide2.QtCore import (QCoreApplication, QPropertyAnimation, QDate, QDateTime, QMetaObject, QObject, QPoint, QRect, QSize, QTime, QUrl, Qt, QEvent)
+from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter, QPixmap, QRadialGradient)
+from PySide2.QtWidgets import *
+from ClassUi_ringProgress import Ui_ringProgress
+from ClassMainWindow import MainWindow
+
+counter = 0
+jumper = 10
+
+class SplashLoad(QtWidgets.QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.ui = Ui_ringProgress()
+        self.ui.setupUi(self)
+
+        self.progressBarValue(0)
+
+        self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+
+        self.shadow = QGraphicsDropShadowEffect(self)
+        self.shadow.setBlurRadius(20)
+        self.shadow.setXOffset(0)
+        self.shadow.setYOffset(0)
+        self.shadow.setColor(QColor(0, 0, 0, 120))
+        self.ui.circularBg.setGraphicsEffect(self.shadow)
+        
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.progress)
+        self.timer.start(15)
+        self.show()
+
+    def progress (self):
+        global counter
+        global jumper
+        value = counter
+        htmlText = """<p><span style=" font-size:68pt;">{VALUE}</span><span style=" font-size:58pt; vertical-align:super;">%</span></p>"""
+        newHtml = htmlText.replace("{VALUE}", str(jumper))
+        if(value >= jumper):
+            self.ui.labelPercentage.setText(newHtml)
+            jumper += 1
+        if value >= 100: value = 1.000
+        self.progressBarValue(value)
+
+        if counter >= 100:
+            self.timer.stop()
+            self.main = MainWindow()
+            self.main.show()
+            self.close()
+        counter += 0.5
+
+    def progressBarValue(self, value):
+        styleSheet = """
+        QFrame{
+        	border-radius: 150px;
+        	background-color: qconicalgradient(cx:0.5, cy:0.5, angle:90, stop:{STOP_1} rgba(255, 0, 127, 0), stop:{STOP_2} rgba(85, 170, 255, 255));
+        }
+        """
+        progress = (100 - value) / 100.0
+        stop_1 = str(progress - 0.001)
+        stop_2 = str(progress)
+        newStylesheet = styleSheet.replace("{STOP_1}", stop_1).replace("{STOP_2}", stop_2)
+        self.ui.circularProgress.setStyleSheet(newStylesheet)
---
+++ Sources/ClassUi_ringProgress.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'ringProgressgxxQqu.ui'
+##
+## Created by: Qt User Interface Compiler version 5.14.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import (QCoreApplication, QDate, QDateTime, QMetaObject,
+    QObject, QPoint, QRect, QSize, QTime, QUrl, Qt)
+from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont,
+    QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter,
+    QPixmap, QRadialGradient)
+from PySide2.QtWidgets import *
+
+
+class Ui_ringProgress(object):
+    def setupUi(self, ringProgress):
+        if not ringProgress.objectName():
+            ringProgress.setObjectName(u"ringProgress")
+        ringProgress.resize(335, 373)
+        self.centralwidget = QWidget(ringProgress)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.circularProgressBarBase = QFrame(self.centralwidget)
+        self.circularProgressBarBase.setObjectName(u"circularProgressBarBase")
+        self.circularProgressBarBase.setGeometry(QRect(10, 10, 320, 320))
+        self.circularProgressBarBase.setFrameShape(QFrame.NoFrame)
+        self.circularProgressBarBase.setFrameShadow(QFrame.Raised)
+        self.circularProgress = QFrame(self.circularProgressBarBase)
+        self.circularProgress.setObjectName(u"circularProgress")
+        self.circularProgress.setGeometry(QRect(10, 10, 300, 300))
+        self.circularProgress.setStyleSheet(u"QFrame {\n"
+"	border-radius: 150px;\n"
+"	background-color: qconicalgradient(cx:0.5, cy:0.5, angle:90, stop:0.749 rgba(255, 0, 127, 0), stop:0.750 rgba(0, 170, 255, 255));\n"
+"}\n"
+"")
+        self.circularProgress.setFrameShape(QFrame.NoFrame)
+        self.circularProgress.setFrameShadow(QFrame.Raised)
+        self.circularBg = QFrame(self.circularProgressBarBase)
+        self.circularBg.setObjectName(u"circularBg")
+        self.circularBg.setGeometry(QRect(10, 10, 300, 300))
+        self.circularBg.setStyleSheet(u"QFrame {\n"
+"	border-radius: 150px;\n"
+"	background-color: rgb(77, 77, 127, 120);\n"
+"}")
+        self.circularBg.setFrameShape(QFrame.NoFrame)
+        self.circularBg.setFrameShadow(QFrame.Raised)
+        self.container = QFrame(self.circularProgressBarBase)
+        self.container.setObjectName(u"container")
+        self.container.setGeometry(QRect(25, 25, 270, 270))
+        self.container.setStyleSheet(u"QFrame {\n"
+"	border-radius: 135px;\n"
+"	background-color: rgb(77, 77, 127);\n"
+"}")
+        self.container.setFrameShape(QFrame.StyledPanel)
+        self.container.setFrameShadow(QFrame.Raised)
+        self.layoutWidget = QWidget(self.container)
+        self.layoutWidget.setObjectName(u"layoutWidget")
+        self.layoutWidget.setGeometry(QRect(10, 50, 261, 181))
+        self.gridLayout = QGridLayout(self.layoutWidget)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.labelTitle = QLabel(self.layoutWidget)
+        self.labelTitle.setObjectName(u"labelTitle")
+        font = QFont()
+        font.setFamily(u"Inter Semi Bold")
+        font.setPointSize(14)
+        self.labelTitle.setFont(font)
+        self.labelTitle.setStyleSheet(u"background-color:none;\n"
+"color: #FFF;")
+        self.labelTitle.setAlignment(Qt.AlignCenter)
+
+        self.gridLayout.addWidget(self.labelTitle, 1, 0, 1, 1)
+
+        self.labelPercentage = QLabel(self.layoutWidget)
+        self.labelPercentage.setObjectName(u"labelPercentage")
+        font1 = QFont()
+        font1.setFamily(u"Lato Light")
+        font1.setPointSize(68)
+        font1.setBold(False)
+        font1.setWeight(50)
+        self.labelPercentage.setFont(font1)
+        self.labelPercentage.setStyleSheet(u"background-color:none;\n"
+"color: #FFF;")
+        self.labelPercentage.setAlignment(Qt.AlignCenter)
+
+        self.gridLayout.addWidget(self.labelPercentage, 2, 0, 1, 1)
+
+        self.circularBg.raise_()
+        self.circularProgress.raise_()
+        self.container.raise_()
+        ringProgress.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(ringProgress)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 335, 29))
+        ringProgress.setMenuBar(self.menubar)
+
+        self.retranslateUi(ringProgress)
+
+        QMetaObject.connectSlotsByName(ringProgress)
+    # setupUi
+
+    def retranslateUi(self, ringProgress):
+        ringProgress.setWindowTitle(QCoreApplication.translate("ringProgress", u"GuiQt", None))
+        self.labelTitle.setText(QCoreApplication.translate("ringProgress", u"<strong>Bernard</strong>", None))
+        self.labelPercentage.setText(QCoreApplication.translate("ringProgress", u"<html><head/><body><p>0<span style=\" font-size:58pt; vertical-align:super;\">%</span></p></body></html>", None))
+    # retranslateUi
+
